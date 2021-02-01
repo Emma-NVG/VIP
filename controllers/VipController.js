9d8b2fb6b0930d68f098bc94781b24e155406143
Emma-NVG <51115377+Emma-NVG@users.noreply.github.com>
2021-02-01 21:57:30 +0100

    Deleting unnecessary commentary

--- controllers/VipController.js
+++ controllers/VipController.js
@@ -3,31 +3,6 @@ let model = require("../models/vip.js");
 
 // ///////////////////////// R E P E R T O I R E    D E S     S T A R S
 
-// module.exports.Repertoire = 	function(request, response) {
-//     response.title = 'Répertoire des stars';
-//     model.repertoireLettre(function (err, result) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         response.lettre = result;
-//         response.render('repertoireVips', response);
-//     });
-// }
-//
-// module.exports.ListPerson = 	function(request, response) {
-//     response.title = 'Liste des stars';
-//     let data = request.params.firstLetter;
-//     model.listPerson(data,function (err, result) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         response.listOfPerson = result;
-//         response.render('repertoireVips', response);
-//     });
-// }
-
 module.exports.Repertoire = 	function(request, response) {
     response.title = 'Répertoire des stars';
     let data = request.params.firstLetter;
