3c10b77fd8e710e6c68323a7cb5eee38d6a5e86d
Emma-NVG <51115377+Emma-NVG@users.noreply.github.com>
2021-02-01 14:27:18 +0100

    Modification of view : repertoireVips.handlebars - addition of letters

--- controllers/VipController.js
+++ controllers/VipController.js
@@ -1,10 +1,16 @@
 
-
+let model = require("../models/vip.js");
 
 // ///////////////////////// R E P E R T O I R E    D E S     S T A R S
 
-module.exports.Repertoire = 	function(request, response){
-   response.title = 'Répertoire des stars';
-
-      response.render('repertoireVips', response);
-  } ;
+module.exports.Repertoire = 	function(request, response) {
+    response.title = 'Répertoire des stars';
+    model.repertoireLettre(function (err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        response.lettre = result;
+        response.render('repertoireVips', response);
+    });
+}
--- models/vip.js
+++ models/vip.js
@@ -11,3 +11,14 @@ module.exports.test = function(callback) {
         }
     });
 };
+
+module.exports.repertoireLettre = function(callback) {
+    db.getConnection(function(err, connexion) {
+        if (!err) {
+            let sql = "SELECT DISTINCT SUBSTR(VIP_NOM,1,1) AS NOM FROM vip ;";
+            // console.log(sql);
+            connexion.query(sql, callback);
+            connexion.release();
+        }
+    });
+};
