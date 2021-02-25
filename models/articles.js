9ea3039a101f87d0821d7e37e87c23814eec9e28
Emma-NVG <51115377+Emma-NVG@users.noreply.github.com>
2021-02-25 08:38:56 +0100

    Bug fixed in articles.js - addition of VIP_NUMERO in sql query

--- models/articles.js
+++ models/articles.js
@@ -13,7 +13,7 @@ module.exports.getVIPs = (callback) => {
 module.exports.getArticleDetails = (data, callback) => {
     db.getConnection((err, connexion) => {
         if (!err) {
-            let sql = "SELECT VIP_NOM AS NAME, VIP_PRENOM AS FIRSTNAME, ARTICLE_TITRE AS TITRE, ARTICLE_RESUME AS CONTENU, ARTICLE_DATE_INSERT AS DATEPUB " +
+            let sql = "SELECT v.VIP_NUMERO AS NUM, VIP_NOM AS NAME, VIP_PRENOM AS FIRSTNAME, ARTICLE_TITRE AS TITRE, ARTICLE_RESUME AS CONTENU, ARTICLE_DATE_INSERT AS DATEPUB " +
                 "FROM article a " +
                 "INNER JOIN apoursujet s ON a.ARTICLE_NUMERO = s.ARTICLE_NUMERO " +
                 "INNER JOIN vip v ON s.VIP_NUMERO = v.VIP_NUMERO " +
